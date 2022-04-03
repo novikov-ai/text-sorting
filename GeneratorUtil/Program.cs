8f26cf8b2ae5047633582ff53ec70f8d90646b96
Aleksandr <al.novikov08@gmail.com>
2022-04-04 00:45:47 +0300

    first commit

---
+++ GeneratorUtil/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace GeneratorUtil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            
+            Console.WriteLine(@"Default generating file is 1000MB (1GB).
+If you want change it provide value in MB or enter any key.");
+            if (!int.TryParse(Console.ReadLine(), out var fileSizeMb))
+            {
+                fileSizeMb = 1000;
+            }
+
+            var filePath = $"../../../../TxtFiles/generated_mb{fileSizeMb}.txt";
+            Console.WriteLine($@"Default file path is: {filePath}");
+
+            var sentenceGeneratorUtil = new SentenceGenerator(filePath);
+            
+            sentenceGeneratorUtil.Generate(fileSizeMb);
+            
+            stopwatch.Stop();
+            
+            Console.WriteLine($@"Text file was generated.
+Time consumed: {stopwatch.Elapsed}");
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GeneratorUtil/SentenceGenerator.cs
@@ -0,0 +1,95 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace GeneratorUtil
+{
+    public class SentenceGenerator
+    {
+        private const string RecordSeparator = ". ";
+
+        private readonly string _path = "../../../../TxtFiles/generated.txt";
+
+        private static Random _random;
+
+        private const string DictionaryPathNouns = "../../../Dictionaries/nouns.txt";
+        private const string DictionaryPathAdverbs = "../../../Dictionaries/adverbs.txt";
+        private const string DictionaryPathAdjectives = "../../../Dictionaries/adjectives.txt";
+
+        private readonly string[] _nouns;
+        private readonly string[] _adverbs;
+        private readonly string[] _adjectives;
+
+        public SentenceGenerator(string path = null)
+        {
+            if (!string.IsNullOrEmpty(path))
+                _path = path;
+
+            _random = new Random();
+
+            _nouns = File.ReadAllLines(DictionaryPathNouns);
+            _adverbs = File.ReadAllLines(DictionaryPathAdverbs);
+            _adjectives = File.ReadAllLines(DictionaryPathAdjectives);
+
+            if (!_nouns.Any() || !_adverbs.Any() || !_adjectives.Any())
+            {
+                throw new ArgumentException($@"Dictionaries wasn't found. 
+
+Check if dictionaries exists:
+- Nouns path: {DictionaryPathNouns}
+- Adverbs path: {DictionaryPathAdverbs}
+- Adjectives path: {DictionaryPathAdjectives}");
+            }
+        }
+
+        public void Generate(long mb)
+        {
+            var byteSum = 1024 * 1024 * mb;
+
+            if (byteSum < 0)
+                throw new ArgumentException("Provide less size.");
+
+            var random = new Random();
+
+            const int BufferSize = 65536;
+
+            using (var sw = new StreamWriter(_path, true, Encoding.UTF8, BufferSize))
+            {
+                string cachedSentence = null;
+
+                while (byteSum > 0)
+                {
+                    var generatedSentence = GenerateSentence();
+
+                    if (cachedSentence != null && byteSum % 9 == 0)
+                    {
+                        generatedSentence = cachedSentence;
+                    }
+
+                    var generatedLine = string.Join(RecordSeparator, new string[2]
+                    {
+                        random.Next(1, 1000).ToString(),
+                        generatedSentence
+                    });
+
+                    sw.WriteLine(generatedLine);
+
+                    byteSum = byteSum - generatedLine.Length - 1;
+
+                    cachedSentence = generatedSentence;
+                }
+            }
+        }
+
+        private string GenerateSentence()
+        {
+            return $"{GetRandomWord(_nouns)} is {GetRandomWord(_adverbs)} {GetRandomWord(_adjectives)}";
+        }
+
+        private static string GetRandomWord(string[] dictionary)
+        {
+            return dictionary[_random.Next(1, dictionary.Length - 1)];
+        }
+    }
+}
